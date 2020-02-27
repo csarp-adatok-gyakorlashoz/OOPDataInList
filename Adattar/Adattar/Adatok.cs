2a1d6439c08fa4f19ecdacb0bb2b01b985bb8407
Gyuris Csaba <gycsaba@vasvari.hu>
2020-02-27 17:38:43 +0100

    Ponatosítás a Person listába

--- Adattar/Adattar/Adatok.cs
+++ Adattar/Adattar/Adatok.cs
@@ -20821,11 +20821,12 @@ namespace ListakbanLevoAdatokKezeleseOOPalapokon
             listPersonsInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork, NY", 45));
             listPersonsInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton, OH", 35));
             listPersonsInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork, NY", 85));
-            listPersonsInCity.Add(new Person("203456882", "Winston", "1208 Alex St, Newyork, NY", 65));
+            listPersonsInCity.Add(new Person("203456882", "Winston", "1208 Alex St, Newyork, NY", 75));
             listPersonsInCity.Add(new Person("203456883", "Mac", "12 Main Street, Newyork, NY", 35));
             listPersonsInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore, NY", 85));
             listPersonsInCity.Add(new Person("203456885", "Elan", "12 Main Street, Newyork, NY", 15));
             listPersonsInCity.Add(new Person("203456886", "Sue", "1208 Alex St, Newyork, NY", 85));
+            listPersonsInCity.Add(new Person("203456878", "Mac", "14 Main Street, Newyork, NY", 75));
         }
 
         public List<Person> getPersons()
